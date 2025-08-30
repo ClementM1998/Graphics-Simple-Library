b5d287594b68cc803a39138a32e65897db3907e7
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-08-30 21:49:07 +0800

    Update Demo.java

--- com/clay/test/Demo.java
+++ com/clay/test/Demo.java
@@ -5,15 +5,51 @@ import static com.clay.gsl.Graphics.*;
 public class Demo {
     public static void main(String[] args) {
 
-        initgraph("Demo");
+        initgraph("Demo Graphics", 800, 600);
 
-        setcolor(RED);
+        while (true) {
+            cleargraph();
 
-        arc(100, 100, 100, 200, 180, 70);
+            if (iskeydown(KEY_ESCAPE)) {
+                closegraph();
+            }
 
-        circle(0, 0, 50);
+            setcolor(WHITE);
+            text("Press 'ESC' to exit", 0, 0);
 
-        rectangle(0, 0, 50, 50);
+            // Tetapkan warna merah 100% opaque
+            setcolor(0xFF0000, 255);
+            line(50, 50, 200, 50);
+
+            // Hijau dengan alpha 128
+            setcolor(0x00FF00, 128);
+            drawrect(100, 100, 200, 150);
+
+            // Biru penuh
+            setcolor(0x0000FF, 255);
+            drawcircle(400, 200, 80);
+
+            // Ungu arc
+            setcolor(0x8800FF, 200);
+            drawarc(400, 400, 0, 180, 100, 50);
+
+            // Hitam dot
+            setcolor(0x000000, 255);
+            point(600, 100);
+
+            // Orange ellipse
+            setcolor(0xFFA500, 200);
+            drawellipse(600, 300, 100, 60);
+
+            // Polygon bentuk bintang
+            int[] pts = {100,400, 140,460, 200,460, 150,500, 170,560, 100,520, 30,560, 50,500, 0,460, 60,460};
+            setcolor(0xFFD700, 255);
+            drawpolygon(pts.length/2, pts);
+
+            refreshgraph();
+        }
+
+        // Biarkan window terbuka
 
     }
 }
