e8a1ae0fe07da2415883b1d497263708c8e96f9e
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-08-31 17:50:37 +0800

    Create DemoGetch.java

---
+++ com/clay/test/DemoGetch.java
@@ -0,0 +1,20 @@
+package com.clay.test;
+
+import static com.clay.gsl.Graphcis.*;
+
+public class DemoGetch {
+    public static void main(String[] args) {
+        initgraph("Demo Getch");
+        cleargraph();
+
+        setcolor(RED);
+        circle(320, 240, 100);
+
+        setcolor(WHITE);
+        outtextxy(250, 450, "Tekan apa-apa key untuk keluar ...");
+
+        while (!isKB()) refreshgraph();
+
+        closegraph();
+    }
+}
