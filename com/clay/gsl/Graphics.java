b09a2d787eeccea26819b460837de540499e8537
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-09-02 21:03:14 +0800

    Update Graphics.java

--- com/clay/gsl/Graphics.java
+++ com/clay/gsl/Graphics.java
@@ -55,6 +55,33 @@ public final class Graphics {
     public static final int KEY_F8 = KeyEvent.VK_F8;
     public static final int KEY_F9 = KeyEvent.VK_F9;
 
+    public static final int KEY_A = KeyEvent.VK_A;
+    public static final int KEY_B = KeyEvent.VK_B;
+    public static final int KEY_C = KeyEvent.VK_C;
+    public static final int KEY_D = KeyEvent.VK_D;
+    public static final int KEY_E = KeyEvent.VK_E;
+    public static final int KEY_F = KeyEvent.VK_F;
+    public static final int KEY_G = KeyEvent.VK_G;
+    public static final int KEY_H = KeyEvent.VK_H;
+    public static final int KEY_I = KeyEvent.VK_I;
+    public static final int KEY_J = KeyEvent.VK_J;
+    public static final int KEY_K = KeyEvent.VK_K;
+    public static final int KEY_L = KeyEvent.VK_L;
+    public static final int KEY_M = KeyEvent.VK_M;
+    public static final int KEY_N = KeyEvent.VK_N;
+    public static final int KEY_O = KeyEvent.VK_O;
+    public static final int KEY_P = KeyEvent.VK_P;
+    public static final int KEY_Q = KeyEvent.VK_Q;
+    public static final int KEY_R = KeyEvent.VK_R;
+    public static final int KEY_S = KeyEvent.VK_S;
+    public static final int KEY_T = KeyEvent.VK_T;
+    public static final int KEY_U = KeyEvent.VK_U;
+    public static final int KEY_V = KeyEvent.VK_V;
+    public static final int KEY_W = KeyEvent.VK_W;
+    public static final int KEY_X = KeyEvent.VK_X;
+    public static final int KEY_Y = KeyEvent.VK_Y;
+    public static final int KEY_Z = KeyEvent.VK_Z;
+
     // Tetikus
     public static final int MOUSE_BUTTON_LEFT = MouseEvent.BUTTON1;
     public static final int MOUSE_BUTTON_MIDDLE = MouseEvent.BUTTON2;
