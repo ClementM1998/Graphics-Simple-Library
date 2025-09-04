63768aa0ffcdbb5661fb931f28c5a11887073de0
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2025-09-04 21:54:18 +0800

    Update DemoControl.java

--- com/clay/test/DemoControl.java
+++ com/clay/test/DemoControl.java
@@ -12,15 +12,23 @@ public class DemoControl {
         while (true) {
             cleargraph();
 
+            char k = getch();
+
             if (iskeydown(KEY_ESCAPE)) closegraph();
+            if (k == 'q') closegraph();
 
             if (iskeydown(KEY_LEFT)) x -= 2;
             if (iskeydown(KEY_UP)) y -= 2;
             if (iskeydown(KEY_RIGHT)) x += 2;
             if (iskeydown(KEY_DOWN)) y += 2;
 
+            if (k == 'a') x -= 2;
+            if (k == 'w') y -= 2;
+            if (k == 'd') x += 2;
+            if (k == 's') y += 2;
+
             setcolor(RED);
-            drawcircle(x, y, 40);
+            circle(x, y, 40);
 
             refreshgraph();
         }
